49e15edc2dd355c3135e850e95d7fb24db02e3df
Your Name <you@example.com>
2023-01-16 09:11:42 +0530

    Added: Find max float value from 3 values

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -2,7 +2,17 @@ package com.bridgelabz;
 
 public class FindMaximum {
 
-    public static Float maxOfFloat(Float x,Float y,Float z) {
+    public static Integer maxOfInteger(Integer x,Integer y,Integer z) {
+
+        Integer max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
+        return max;
+
+    }
+    public Float maxOfFloat(Float x,Float y,Float z) {
 
         Float max = x;
         if (y.compareTo(max) > 0)
@@ -16,7 +26,9 @@ public class FindMaximum {
     public static void main(String[] args) {
 
         System.out.println("Welcome To Find Maximum number Program...");
-        System.out.println("Maximum Float value between 3 values : "+maxOfFloat(3.1f,8.5f,9.6f));
+        System.out.println("Maximum Integer value between 3 values : "+maxOfInteger(3,8,9));
+        FindMaximum findMaximum = new FindMaximum();
+        System.out.println("Maximum Float value between 3 values : "+findMaximum.maxOfFloat(3.1f,8.5f,9.6f));
 
     }
 }
