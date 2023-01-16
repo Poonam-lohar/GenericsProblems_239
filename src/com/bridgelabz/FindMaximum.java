7937c6b954c976d9ad371537067d3fa9675ff5c8
Your Name <you@example.com>
2023-01-16 09:24:49 +0530

    Added: Refactor all 3 to one generic method

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -2,9 +2,9 @@ package com.bridgelabz;
 
 public class FindMaximum {
 
-    public static Integer maxOfInteger(Integer x,Integer y,Integer z) {
+    public static <E extends Comparable<E>> E maxOfValue(E x,E y,E z) {
 
-        Integer max = x;
+        E max = x;
         if (y.compareTo(max) > 0)
             max = y;
         if (z.compareTo(max) > 0)
@@ -12,37 +12,13 @@ public class FindMaximum {
         return max;
 
     }
-    public Float maxOfFloat(Float x,Float y,Float z) {
-
-        Float max = x;
-        if (y.compareTo(max) > 0)
-            max = y;
-        if (z.compareTo(max) > 0)
-            max = z;
-        return max;
-
-    }
-
-    public String maxOfString(String x,String y,String z) {
-
-        String max = x;
-        if (y.compareTo(max) > 0)
-            max = y;
-        if (z.compareTo(max) > 0)
-            max = z;
-        return max;
-
-    }
-
-
-
     public static void main(String[] args) {
 
         System.out.println("Welcome To Find Maximum number Program...");
-        System.out.println("Maximum Integer value between 3 values : "+maxOfInteger(3,8,9));
+        System.out.println("Maximum Integer value between 3 values : "+maxOfValue(3,8,9));
         FindMaximum findMaximum = new FindMaximum();
-        System.out.println("Maximum Float value between 3 values : "+findMaximum.maxOfFloat(3.1f,8.5f,9.6f));
-        System.out.println("Maximum String value between 3 values: "+findMaximum.maxOfString("Apple","Peach","Banana"));
+        System.out.println("Maximum Float value between 3 values : "+findMaximum.maxOfValue(3.1f,8.5f,9.6f));
+        System.out.println("Maximum String value between 3 values: "+findMaximum.maxOfValue("Apple","Peach","Banana"));
 
 
     }
