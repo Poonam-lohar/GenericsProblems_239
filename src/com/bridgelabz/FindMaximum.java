c316006f2acb9b80bb4048f40e4385a9e1b665e9
Your Name <you@example.com>
2023-01-16 09:16:45 +0530

    Added: Find max String from 3 string

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -23,12 +23,27 @@ public class FindMaximum {
 
     }
 
+    public String maxOfString(String x,String y,String z) {
+
+        String max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
+        return max;
+
+    }
+
+
+
     public static void main(String[] args) {
 
         System.out.println("Welcome To Find Maximum number Program...");
         System.out.println("Maximum Integer value between 3 values : "+maxOfInteger(3,8,9));
         FindMaximum findMaximum = new FindMaximum();
         System.out.println("Maximum Float value between 3 values : "+findMaximum.maxOfFloat(3.1f,8.5f,9.6f));
+        System.out.println("Maximum String value between 3 values: "+findMaximum.maxOfString("Apple","Peach","Banana"));
+
 
     }
 }
